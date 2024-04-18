8aba2064f74489df16d7aff12513dc186d618b55
justinknguyen <justin.nguyen@ecapital.com>
2024-04-17 18:00:19 -0600

    update global_hotkeys module

--- BeatBind/global_hotkeys/__init__.py
+++ BeatBind/global_hotkeys/__init__.py
@@ -1,10 +1,70 @@
 from .hotkey_checker import hotkey_checker
 
-def register_hotkey(key, modifiers, press_callback, release_callback=None):
-	return hotkey_checker.register_hotkey(key, modifiers, press_callback, release_callback)
+def _syntax_check(binding):
+    if(isinstance(binding, list)):
+        hotkey_string = str(binding)
+        valid_hotkey_string = " + ".join(binding)
+        raise Exception(
+            "You've specified the hotkey as a list. The syntax has changed to being specified as a string now.\n"+
+            f"Your hotkey {hotkey_string} should now be specified as \"{valid_hotkey_string}\""
+        )
 
-def remove_hotkey(key, modifiers):
-	return hotkey_checker.remove_hotkey(key, modifiers)
+def retrofit_old_bindings(binding):
+     if isinstance(binding, list):
+          return " + ".join(binding)
+     return binding
+
+def sanitize_binding(binding_raw):
+    if (isinstance(binding_raw, list)):
+        _binding = retrofit_old_bindings(binding_raw[0])
+        keydown_function = binding_raw[1]
+        keyup_function = binding_raw[2]
+        actuate_on_partial_release = True
+        if len(binding_raw) > 3:
+            actuate_on_partial_release = binding_raw[3]
+        press_params = None
+        release_params = None
+        if len(binding_raw) > 4:
+            press_params = binding_raw[4]
+            release_params = binding_raw[4]
+        if len(binding_raw) > 5:
+            release_params = binding_raw[5]
+        return [_binding, keydown_function, keyup_function, actuate_on_partial_release, press_params, release_params]
+    elif (isinstance(binding_raw, dict)):
+        _binding = retrofit_old_bindings(binding_raw["hotkey"])
+        keydown_function = binding_raw["on_press_callback"]
+        keyup_function = binding_raw["on_release_callback"]
+        actuate_on_partial_release = True
+        if "actuate_on_partial_release" in binding_raw:
+            actuate_on_partial_release = binding_raw["actuate_on_partial_release"]
+        press_params = None
+        release_params = None
+        if "callback_params" in binding_raw:
+            press_params = binding_raw["callback_params"]
+            release_params = binding_raw["callback_params"]
+        if "press_callback_params" in binding_raw:
+            press_params = binding_raw["press_callback_params"]
+        if "release_callback_params" in binding_raw:
+            release_params = binding_raw["release_callback_params"]
+        return [_binding, keydown_function, keyup_function, actuate_on_partial_release, press_params, release_params]
+    else:
+        binding_raw_str = str(binding_raw)
+        raise Exception(f"Binding {binding_raw_str} is not the correct_type")
+
+def register_hotkey(binding, press_callback, release_callback, actuate_on_partial_release = False, press_params = None, release_params = "0e7ef7c6-d3dc-4947-9d7c-5a8f7eb3e1e9"):
+    _syntax_check(binding)
+    press__params = press_params
+    release__params = press_params
+    # Using a UUID here as a workaround to differentiate from None being passed.
+    if release_params != "0e7ef7c6-d3dc-4947-9d7c-5a8f7eb3e1e9":
+        release__params = release_params
+    return hotkey_checker.register_hotkey([hotkey.split("+") for hotkey in binding.replace(" ","").split(",")], press_callback, release_callback, actuate_on_partial_release, press__params, release__params)
+ 
+def _register_hotkey(binding, press_callback, release_callback, actuate_on_partial_release, press_params, release_params):
+	return hotkey_checker.register_hotkey(binding, press_callback, release_callback, actuate_on_partial_release, press_params, release_params)
+
+def remove_hotkey(binding):
+	return hotkey_checker.remove_hotkey(binding)
 
 def start_checking_hotkeys():
 	hotkey_checker.start_checking_hotkeys()
@@ -16,26 +76,16 @@ def restart_checking_hotkeys():
 	hotkey_checker.restart_checker()
 
 def register_hotkeys(bindings):
-    for binding, keydown_function, keyup_function in bindings:
-        key = binding[-1]
-        modifiers = []
-        for i in range(0, len(binding) - 1):
-            modifiers.append(binding[i])
-
-        register_hotkey(key, modifiers, keydown_function, keyup_function)
+    for binding_raw in bindings:
+        _binding, keydown_function, keyup_function, actuate_on_partial_release, press_params, release_params = sanitize_binding(binding_raw)
+        _syntax_check(_binding)
+        binding = [hotkey.split("+") for hotkey in _binding.replace(" ","").split(",")]
+        _register_hotkey(binding, keydown_function, keyup_function, actuate_on_partial_release, press_params, release_params)
 
 def remove_hotkeys(bindings):
-    for binding in bindings:
-        # To accommodate simply reusing the full binding definition ([key_combo], key-down_handler, key-up handler), we'll get just the keybinding.
-        if isinstance(binding[0], list):
-            binding = binding[0]
-        
-        key = binding[-1]
-        modifiers = []
-        for i in range(0, len(binding) - 1):
-            modifiers.append(binding[i])
-
-        remove_hotkey(key, modifiers)
+    for _binding in bindings:
+        binding = [hotkey.split("+") for hotkey in _binding.replace(" ","").split(",")]
+        remove_hotkey(binding)
 
 # Remove all keybindings.
 # *Note that this also stops the hotkey checking thread.
--- BeatBind/global_hotkeys/hotkey_checker.py
+++ BeatBind/global_hotkeys/hotkey_checker.py
@@ -6,13 +6,23 @@ from ctypes import wintypes
 import win32con
 import win32api
 
-from .keycodes import vk_key_names
+from .keycodes import vk_key_names, vk_non_modifier_codes
+
 
 def _to_virtualkey(key):
     virtual_key = None
     if key.lower() in vk_key_names.keys():
         virtual_key = vk_key_names[key.lower()]
     return virtual_key
+
+full_modifier_list = [
+    _to_virtualkey("control"),
+    _to_virtualkey("shift"),
+    _to_virtualkey("alt"),
+    _to_virtualkey("left_window"),
+    _to_virtualkey("right_window"),
+    _to_virtualkey("window"),
+]
 
 class EngineState:
 
@@ -28,16 +38,10 @@ class HotkeyChecker():
         self.state = EngineState()
         self.hotkey_counter = 0
     
-    def _find_hotkey_id(self, hotkey):
-        for hotkey_id, hotkey_modifiers in self.hotkeys.items():
-            all_match = False
-            if len(hotkey_modifiers) == len(hotkey):
-                all_match = True
-                for modifier in hotkey:
-                    if modifier not in hotkey_modifiers:
-                        all_match = False
-                        break
-            if all_match:
+    def _find_hotkey_id(self, binding):
+        for hotkey_id, _binding in self.hotkeys.items():
+            match = False
+            if _binding == binding:
                 return hotkey_id
         return None
 
@@ -59,17 +63,28 @@ class HotkeyChecker():
         self.hotkeys.clear()
         self.hotkey_actions.clear()
         self.hotkey_counter = 0
-    
-    def remove_hotkey(self, key, modifiers):
-        virtual_key = _to_virtualkey(key)
 
-        # If the key doesn't exist, throw an exception to bring attention to it.
-        if virtual_key == None:
-            raise Exception(
-                "The key [%s] not a valid virtual keystroke." % key)
-            return False
+    def _is_valid_binding(self, binding):
+        for chord in binding:
+            for key in chord:
+                virtual_key = _to_virtualkey(key)
+                # If the key doesn't exist, throw an exception to bring attention to it.
+                if virtual_key == None:
+                    if(isinstance(key, list)):
+                        hotkey_string = str(key)
+                        valid_hotkey_string = " + ".join(key)
+                        raise Exception(
+                            "You've specified the hotkey as a list. The syntax has changed to being specified as a string now.\n"+
+                            f"Your hotkey {hotkey_string} should now be specified as \"{valid_hotkey_string}\""
+                        )
+                    raise Exception(
+                        "The key [%s] not a valid virtual keystroke." % key)
+                    return False
+    
+    def remove_hotkey(self, binding):
+        self._is_valid_binding(binding)
         
-        hotkey_id = self._find_hotkey_id(modifier_list)
+        hotkey_id = self._find_hotkey_id(binding)
         if hotkey_id == None:
             return False
         
@@ -78,29 +93,90 @@ class HotkeyChecker():
 
         return True
 
-    def register_hotkey(self, key, modifiers, press_callback, release_callback=None):
-        virtual_key = _to_virtualkey(key)
+    def _reset_binding_press_state(self, hotkey_id):
+        for i in range(0, len(self.hotkey_actions[hotkey_id][2])):
+            self.hotkey_actions[hotkey_id][2][i] = 0
 
-        # If the key doesn't exist, throw an exception to bring attention to it.
-        if virtual_key == None:
-            raise Exception(
-                "The key [%s] not a valid virtual keystroke." % key)
-            return False
+    def register_hotkey(self, binding, press_callback, release_callback, actuate_on_partial_release, press_callback_params, release_callback_params):
+        self._is_valid_binding(binding)
         
         self.hotkey_counter += 1
         id = self.hotkey_counter
 
-        modifier_list = [virtual_key, ]
-        for modifier in modifiers:
-            modifier_list.append(vk_key_names[modifier.lower()])
-
-        if self._find_hotkey_id(modifier_list) != None:
+        hotkey_id = self._find_hotkey_id(binding)
+        if hotkey_id != None:
+            raise Exception(
+                "The hotkey [%s] is already registered." % str(binding)
+            )
             return False
 
-        self.hotkeys[id] = tuple(modifier_list)
-        self.hotkey_actions[id] = [press_callback, release_callback, False]
+        # we want to track how far along the binding's chording sequence we've progressed.
+        binding_press_state = [0 for i in range(0, len(binding))]
+
+        self.hotkeys[id] = binding
+        self.hotkey_actions[id] = [press_callback, release_callback, binding_press_state, actuate_on_partial_release, press_callback_params, release_callback_params]
 
         return True
+    
+    def _find_index_of_first_item_not_matching_in_list(self, _list, target):
+        for i in range(0, len(_list)):
+            if _list[i] != target:
+                return i
+        return None
+
+    def _are_all_keys_pressed_in_chord(self, chord):
+        for key in chord:
+            if key == "window":
+                lwin_key_state = win32api.GetAsyncKeyState(win32con.VK_LWIN)
+                rwin_key_state = win32api.GetAsyncKeyState(win32con.VK_RWIN)
+                if (lwin_key_state >= 0) and (rwin_key_state >= 0):
+                    return False
+            else:
+                specific_key_state = win32api.GetAsyncKeyState(key)
+                if specific_key_state >= 0:
+                    return False
+        return True
+    
+    def _are_any_keys_pressed_in_chord(self, chord):
+        for key in chord:
+            if key == "window":
+                continue
+            else:
+                specific_key_state = win32api.GetAsyncKeyState(key)
+                if specific_key_state < 0:
+                    return True
+        return False
+    
+    def _are_all_keys_not_pressed_in_chord(self, chord):
+        for key in chord:
+            if key == "window":
+                lwin_key_state = win32api.GetAsyncKeyState(win32con.VK_LWIN)
+                rwin_key_state = win32api.GetAsyncKeyState(win32con.VK_RWIN)
+                if (lwin_key_state < 0) or (rwin_key_state < 0):
+                    return False
+            else:
+                specific_key_state = win32api.GetAsyncKeyState(key)
+                if specific_key_state < 0:
+                    return False
+        return True
+
+    def _get_chord_state(self, chord):
+        result = {}
+        #chord = [_to_virtualkey(key) if key != "window" else "window" for key in _chord]
+        for _key in chord:
+            key = _to_virtualkey(_key) if _key != "window" else "window"
+            result[_key] = str(False)
+            print(_key)
+            if key == "window":
+                lwin_key_state = win32api.GetAsyncKeyState(win32con.VK_LWIN)
+                rwin_key_state = win32api.GetAsyncKeyState(win32con.VK_RWIN)
+                if (lwin_key_state < 0) or (rwin_key_state < 0):
+                    result[_key] = str(True)
+            else:
+                specific_key_state = win32api.GetAsyncKeyState(key)
+                if specific_key_state < 0:
+                    result[_key] = str(True)
+        return result
 
     def run(self):
         state = self.state
@@ -111,28 +187,64 @@ class HotkeyChecker():
             # Exit out if the main thread has terminated.
             if not main_thread().is_alive():
                 break
-            
+
             for id in id_list:
                 hotkey = self.hotkeys[id]
-                press_callback, release_callback, key_state = self.hotkey_actions[id]
+                press_callback, release_callback, binding_press_state, actuate_on_partial_release, press_callback_params, release_callback_params = self.hotkey_actions[id]
+                key_state_id = self._find_index_of_first_item_not_matching_in_list(binding_press_state, 2)
+                if(key_state_id is None):
+                    raise Exception("binding_press_state was not reset after completion!")
+                    continue
+                key_state = binding_press_state[key_state_id]
+                chord = [_to_virtualkey(key) if key != "window" else "window" for key in hotkey[key_state_id]]
 
+                non_allowed_keys = [key for key in vk_non_modifier_codes if key not in chord]
+
+                # Check to see if an active chord is being broken
+                if((key_state_id > 0) and (self._are_any_keys_pressed_in_chord(non_allowed_keys))):
+                    self._reset_binding_press_state(id)
+                    continue
                 # check to see if all keys in the hotkey are pressed.
-                pressed = True
-                for key in hotkey:
-                    specific_key_state = win32api.GetAsyncKeyState(key)
-                    if specific_key_state >= 0:
-                        pressed = False
-                        break
+                pressed = self._are_all_keys_pressed_in_chord(chord)
+                fully_not_pressed = self._are_all_keys_not_pressed_in_chord(chord)
+                
+                # ensure that modifiers not in this hotkey aren't pressed
+                non_allowed_modifiers = []
+                for key in full_modifier_list:
+                    if key not in chord:
+                        if((key == "window") and ((_to_virtualkey("left_window") in chord) or _to_virtualkey("right_window") in chord)):
+                            continue
+                        if(key == _to_virtualkey("left_window")) and ("window" in chord):
+                            continue
+                        if(key == _to_virtualkey("right_window")) and ("window" in chord):
+                            continue
+                        non_allowed_modifiers.append(key)
                 
                 if pressed:
-                    self.hotkey_actions[id][2] = True
-                    if not key_state:
+                    pressed = self._are_all_keys_not_pressed_in_chord(non_allowed_modifiers)
+
+                if pressed:
+                    this_is_the_last_chord = key_state_id == len(hotkey) - 1
+                    self.hotkey_actions[id][2][key_state_id] = 1
+                    if (key_state != 1) and (this_is_the_last_chord):
                         if press_callback != None:
-                            press_callback()
+                            if press_callback_params != None:
+                                press_callback(press_callback_params)
+                            else:
+                                press_callback()
                 else:
-                    self.hotkey_actions[id][2] = False
-                    if key_state:
-                        if release_callback != None:
-                            release_callback()
+                    this_is_the_last_chord = key_state_id == len(hotkey) - 1
+                    #self.hotkey_actions[id][2] = False
+                    if (key_state == 1):
+                        if this_is_the_last_chord:
+                            if fully_not_pressed or actuate_on_partial_release:
+                                self._reset_binding_press_state(id)
+                                if release_callback != None:
+                                    if release_callback_params != None:
+                                        release_callback(release_callback_params)
+                                    else:
+                                        release_callback()
+                        else:
+                            self.hotkey_actions[id][2][key_state_id] = 2
 
 hotkey_checker = HotkeyChecker()
